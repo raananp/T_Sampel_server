216c8caf31b06803599832fc1f4e95b24a8dbcb4
Raanan Peretz <47702617+raananp@users.noreply.github.com>
2022-02-24 13:52:09 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,136 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_network_interface" "nic" {
+  subnet_id       = aws_subnet.main.id 
+  private_ips     = ["172.0.2.50"]
+  security_groups = [aws_security_group.allow_ssh.id]
+
+}
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.nic.id
+  associate_with_private_ip = "172.0.2.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_instance" "Terraformtest" {
+  ami = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  //subnet_id = aws_subnet.main.id
+  availability_zone = "us-east-1a"
+  key_name = "ServerTestAWSVerginaia"
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.nic.id
+
+  }
+  tags = {
+    "Name" = "terraform"
+  }
+ user_data = <<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo bash -c 'echo new server > /var/www/html/index.html'
+EOF
+
+}
+
